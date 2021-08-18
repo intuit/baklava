0e029097983db6cea00a7d779b887b149975fbc4
Deepen Mehta <mehtadeepen@users.noreply.github.com>
2021-08-17 17:55:53 -0700

    Update setup.py (#12)
    * Update setup.py
    
    * fix tox.ini
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    * Update python-package.yml
    
    * Update tox.ini
    
    * fix tox.ini
    
    * fix tox.ini
    
    * fix tox.ini
    
    Co-authored-by: Deepen Mehta <deepen_mehta@intuit.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
 
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='baklava',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     use_scm_version={
+        'local_scheme': 'no-local-version',
         'write_to': 'baklava/__version.py',
         'write_to_template': '__version__ = \'{version}\''
     },
